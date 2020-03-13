f9491d9496d1bc6eb95e41570819089af9bff295
Kenneth Solomon <synecders@gmail.com>
2020-03-13 12:21:13 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,5 @@
+<?php include_once 'includes/components/header.php' ?>
+<div class="container">
+
+</div>
+<?php include_once 'includes/components/footer.php' ?>
---
+++ includes/components/footer.php
@@ -0,0 +1,7 @@
+</body>
+
+<script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+</html>
