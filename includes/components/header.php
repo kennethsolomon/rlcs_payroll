a1c8e4e26813331ec089c2769e83b395add4241a
Kenneth Solomon <synecders@gmail.com>
2020-03-13 12:25:34 +0800

    add classess

--- includes/components/header.php
+++ includes/components/header.php
@@ -1,3 +1,5 @@
+<?php include_once 'includes/config.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+ob_start(); //Turns on output buffering 
+session_start();
+
+date_default_timezone_set("Asia/Manila");
+
+try {
+    $con = new PDO("mysql:dbname=rlcs_payroll;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
